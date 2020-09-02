ecbe6fea34a8ae7a468cab5ae6e2b524dea2afe1
GOI17 <olivasgilberto@gmail.com>
2020-09-01 17:04:41 -0700

    :sparkles: First Commit

---
+++ mysite/polls/urls.py
@@ -0,0 +1,15 @@
+from typing import Pattern
+from django.urls import path
+
+from . import views
+
+# application namespace
+app_name = 'polls'
+
+# application urls
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
